4443833d80f7eab6e7447bce82aaa358c60e6494
sschaeffer <sschaeffer@hotmail.com>
2022-11-28 19:44:26 +0900

    night commit

--- s2c_linker.py
+++ s2c_linker.py
@@ -1,1 +1,20 @@
-print("Test")
+import os, json
+
+class s2c_linker():
+
+    def __init__(self):
+        self.config = None 
+        self.read_configuration("./config.json")
+
+    def read_configuration(self,config_file):
+        if os.path.isfile(config_file) and os.access(config_file, os.R_OK):
+            with open(config_file, "r") as jsonfile:
+                self.config = json.load(jsonfile)
+            jsonfile.close()
+
+    def show_configuration(self):
+        print(self.config["sleep"])
+
+if __name__ == "__main__":
+    S2C = s2c_linker()
+    S2C.show_configuration()
